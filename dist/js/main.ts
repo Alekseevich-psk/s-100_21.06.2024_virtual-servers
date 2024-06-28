d773147b2d77c1f8ec35892ac90dba5ba0d30ebf
Andrew <ap-pskov@ya.ru>
2024-06-28 18:50:49 +0300

    ➕ add configure section

--- dist/js/main.ts
+++
@@ -1,9 +0,0 @@
-import './swiper.ts';
-import './c-rating.ts';
-import './imask.ts';
-
-import '@Components/preloader/preloader.ts';
-import '@Components/btn-top/btn-top.ts';
-import '@Components/header/header.ts';
-
-import '@Sections/main/main.ts';
